ad736ee2d79e173d34996516bb6e66b225faf2b8
Túlio <tulio.sdg@gmail.com>
2025-03-05 15:07:37 -0300

    feat: primeira página do lit

---
+++ src/streamlit/main.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.title("ASPBr")
+st.markdown("> Análise de Sentimentos e Polaridades para o Português Brasileiro")
+st.markdown("O ASPBr é uma aplicação web que permite realizar a tarefa de análise de sentimentos e polaridades em textos em português brasileiro através de diferentes modelos.")
