9f58d616f9820e263afb5075ab1d3a53fd57c39f
Túlio <tulio.sdg@gmail.com>
2025-03-11 15:48:48 -0300

    feat: add logos e autoria

---
+++ src/streamlit/utils.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+def logo_lamid():
+    st.divider()
+    colunas = st.columns([3, 0.5])
+    colunas[0].image('src\streamlit\imgs\lamid-logo-full.png', width=150)
+    colunas[1].caption("Desenvolvido por:<br>Túlio Gois & Nayla Chagas", unsafe_allow_html=True)
