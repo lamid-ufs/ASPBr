f813e580d9a93fa5f10f7dc14dc7fe3757ec1fcc
Túlio <tulio.sdg@gmail.com>
2025-03-11 15:51:20 -0300

    feat: add configuração da página e ícone

--- src/streamlit/main.py
+++ src/streamlit/main.py
@@ -2,6 +2,8 @@ from tabs.analise_sentimentos import tab_analise_sentimentos
 #from tabs.saiba_mais import tab_saiba_mais
 import streamlit as st
 
+st.set_page_config(page_title="ASPBr", page_icon="🔍", layout="wide")
+
 st.title("ASPBr")
 st.markdown("> Análise de Sentimentos e Polaridades para o Português Brasileiro")
 st.markdown("O ASPBr é uma aplicação web que permite realizar a tarefa de análise de sentimentos de textos em português utilizando a biblioteca [_pysentimiento_](https://github.com/pysentimiento/pysentimiento) (Informações sobre a biblioteca estão na aba **'Saiba mais'**).")
