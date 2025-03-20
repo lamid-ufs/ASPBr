77d5cd7c0105679c70e8293eeb4d0503b7744514
Dev <dev@pop-os.mshome.net>
2025-03-19 21:02:23 -0300

    feat: adição de análise defeituosa

--- src/streamlit/tabs/analise_sentimentos.py
+++ src/streamlit/tabs/analise_sentimentos.py
@@ -9,25 +9,61 @@ def load_pysentimiento_analyzer():
     return create_analyzer(task="sentiment", lang="pt")
 
 @st.dialog("Planilha inválida!")
-def erro():
-    st.error("certifique-se de que a planilha inserida possui dados",
-    icon=":material/quick_reference:")
+def erro(tipo):
+    if tipo == "VAZIA":
+        mensagem = "certifique-se de que a planilha inserida possui dados"
+    if tipo == "NULOS":
+        mensagem = "Não é possível analisar a planilha: existem células nulas/vazias no arquivo"
+    st.error(mensagem,
+             icon=":material/quick_reference:")
     
     if st.button("Ok"):
         st.cache_data.clear()
         del st.session_state.arquivo_lido
         st.rerun()
 
+@st.dialog("### Resultado", width = large)
+def analisador_coluna(resultados):
+
+    with st.container(border=True):
+        st.markdown("#### Resultado")
+        st.markdown("A `Polaridade` indica qual é o sentimento predominante no texto e as colunas `'Positiva'`, `'Neutra'`, e `'Negativa'` indicam qual a probabilidade do texto possuir a respectiva polaridade.")
+        
+        for resultado in resultados:
+            st.table(
+                {
+                    #"**Polaridade**": [resultado.output],
+                    "**Positiva**": [resultado.probas['POS']],
+                    "**Neutra**": [resultado.probas['NEU']],
+                    "**Negativa**": [resultado.probas['NEG']]
+                }
+            )
+
+def carregamento():
+    estado = False
+    while not estado:
+        analisador()
+        st.spinner("Analisando...")
+    
+
 
 @st.dialog("Análise de colunas", width = "large")
-def seletor(colunas):
-    st.selectbox(
-        "Que coluna você deseja analisar?",
-        colunas,
-        index=None,
-        placeholder="Selecione a coluna desejada...",
-    )
+def seletor(colunas, dataframe):
+    coluna = st.selectbox(
+            "Que coluna você deseja analisar?",
+            colunas,
+            index=None,
+            placeholder="Selecione a coluna desejada...",
+        )
+    
+    if st.button("Analisar",
+                 key = 'analise_coluna',
+                 help = "Clique para analisar as emoções da coluna selecionada",
+                 on_click = carregamento()):
+        valores = dataframe[coluna].tolist()
 
+        
+    
 def read_planilha(arquivo):
     """
     Centraliza a leitura do arquivo a partir da extensão
@@ -46,12 +82,16 @@ def extrator_colunas(arquivo):
     """
     if arquivo is not None:
         dataframe = read_planilha(arquivo)
+        
+        if dataframe.isnull().values.any():
+            erro("NULOS")
+
         colunas = dataframe.columns
 
         if not colunas.empty:
-            seletor(colunas)
+            seletor(colunas, dataframe)
         else:
-            erro()
+            erro("VAZIA")
             
 
 def analisador(texto):
@@ -59,7 +99,7 @@ def analisador(texto):
     Realiza a análise de sentimentos a partir da pysentimiento.
     """
     analyzer = load_pysentimiento_analyzer()
-    return analyzer.predict(texto)
+    return True, analyzer.predict(texto)
 
 def upload_arquivo():
     """
@@ -80,7 +120,7 @@ def tab_analise_sentimentos():
     st.markdown("Insira o texto na caixa abaixo e clique no botão **Analisar** para obter a análise de sentimentos.")
     texto = st.text_area("Texto", placeholder="Insira o texto a ser analisado...", height=70)
     if st.button("Analisar"):
-        resultado = analisador(texto)
+        _, resultado = analisador(texto)
         with st.container(border=True):
             st.markdown("#### Resultado")
             st.markdown("A `Polaridade` indica qual é o sentimento predominante no texto e as colunas `'Positiva'`, `'Neutra'`, e `'Negativa'` indicam qual a probabilidade do texto possuir a respectiva polaridade.")
